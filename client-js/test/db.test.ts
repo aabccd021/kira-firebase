e18ee9b8ec11120ea54267ab316189c8133b6a2a
aabccd021 <aabccd021@gmail.com>
2021-05-16 19:58:59 +0700

    working time test

--- client-js/test/db.test.ts
+++ client-js/test/db.test.ts
@@ -1,3 +1,6 @@
+import 'jest-extended';
+import 'jest-chain';
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore, useFirestoreEmulator } from 'firebase/firestore/lite';
 
@@ -31,25 +34,25 @@ describe('a', () => {
 
     const user1_0 = await dbpReadDoc({ collection: 'user', id: 'user1' });
     expect(user1_0._tag).toEqual('right');
-    expect(user1_0).toEqual(
-      expect.objectContaining({
-        _tag: 'right',
-        value: expect.objectContaining({
-          data: expect.objectContaining({
-            displayName: 'user1',
-            memeImageCreatedCount: 0,
-            memeCreatedCount: 0,
-            profilePicture: {
-              url: 'https://sakurazaka46.com/images/14/eb2/a748ca8dac608af8edde85b62a5a8/1000_1000_102400.jpg',
-            },
-          }),
-          state: 'exists',
-        }),
-      })
-    );
-    // const user1JoinedTime = user1_0?.['joinedTime'] as firestore.Timestamp;
-    // const timeSinceUser1Joined = new Date().getTime() - user1JoinedTime.toDate().getTime();
-    // expect(timeSinceUser1Joined).toBeGreaterThan(0);
-    // expect(timeSinceUser1Joined).toBeLessThan(7000);
+    expect(user1_0).toEqual({
+      _tag: 'right',
+      value: {
+        data: {
+          displayName: 'user1',
+          memeImageCreatedCount: 0,
+          memeCreatedCount: 0,
+          profilePicture: {
+            url: 'https://sakurazaka46.com/images/14/eb2/a748ca8dac608af8edde85b62a5a8/1000_1000_102400.jpg',
+          },
+          joinedTime: expect.toSatisfy(
+            (x) =>
+              x instanceof Date &&
+              new Date().getTime() - x.getTime() < 7000 &&
+              new Date().getTime() - x.getTime() > 0
+          ),
+        },
+        state: 'exists',
+      },
+    });
   }, 20000);
 });
