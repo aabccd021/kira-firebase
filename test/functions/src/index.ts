f530184b9276beee83e879fef8aafe8a9e334226
aabccd021 <aabccd021@gmail.com>
2021-07-12 17:44:56 +0700

    check

--- test/functions/src/index.ts
+++ test/functions/src/index.ts
@@ -1,5 +1,5 @@
 import { getApp } from './db';
-import { getTriggers_1 as getTriggers, shouldRunTrigger } from 'kira-firebase-server';
+import { Dictionary, getTriggers, shouldRunTrigger,Field } from 'kira-firebase-server';
 import * as functions from 'firebase-functions'
 
 export const finiteUpdate = functions.firestore.document('finite/{docId}').onUpdate(async (snapshot) => {
@@ -8,73 +8,75 @@ export const finiteUpdate = functions.firestore.document('finite/{docId}').onUpd
   }
 })
 
+
+
 export const kira = getTriggers({
   firestore: getApp().firestore(),
-  schema: {
-    version: '0.3.8',
-    userCol: 'user',
-    cols: {
-      user: {
-        displayName: {
-          type: 'string',
-        },
-        memeImageCreatedCount: {
-          type: 'count',
-          countedCol: 'memeImage',
-          groupByRef: 'owner',
-        },
-        memeCreatedCount: {
-          type: 'count',
-          countedCol: 'meme',
-          groupByRef: 'owner',
-        },
-        profilePicture: {
-          type: 'image',
-        },
-        joinedTime: {
-          type: 'creationTime',
+  version: {
+    'kira-core': '0.3.8',
+    'kira-firebase-server': '0.1.8',
+  },
+  cols: {
+    user: {
+      displayName: {
+        type: 'string',
+      },
+      memeImageCreatedCount: {
+        type: 'count',
+        countedCol: 'memeImage',
+        groupByRef: 'owner',
+      },
+      memeCreatedCount: {
+        type: 'count',
+        countedCol: 'meme',
+        groupByRef: 'owner',
+      },
+      profilePicture: {
+        type: 'image',
+      },
+      joinedTime: {
+        type: 'creationTime',
+      },
+    },
+    memeImage: {
+      creationTime: {
+        type: 'creationTime',
+      },
+      image: {
+        type: 'image',
+      },
+      memeCreatedCount: {
+        type: 'count',
+        countedCol: 'meme',
+        groupByRef: 'memeImage',
+      },
+      owner: {
+        type: 'owner',
+        syncFields: {
+          profilePicture: true,
+          displayName: true,
         },
       },
+    },
+    meme: {
       memeImage: {
-        creationTime: {
-          type: 'creationTime',
-        },
-        image: {
-          type: 'image',
-        },
-        memeCreatedCount: {
-          type: 'count',
-          countedCol: 'meme',
-          groupByRef: 'memeImage',
-        },
-        owner: {
-          type: 'owner',
-          syncFields: {
-            profilePicture: true,
-            displayName: true,
-          },
+        type: 'ref',
+        refCol: 'memeImage',
+        syncFields: {
+          image: true,
         },
       },
-      meme: {
-        memeImage: {
-          type: 'ref',
-          refCol: 'memeImage',
-          syncFields: {
-            image: true,
-          },
-        },
-        creationTime: {
-          type: 'creationTime',
-        },
-        text: {
-          type: 'string',
-        },
-        owner: {
-          type: 'owner',
-          syncFields: {
-            profilePicture: true,
-            displayName: true,
-          },
+      creationTime: {
+        type: 'creationTime',
+      },
+      text: {
+        type: 'string',
+      },
+      owner: {
+        type: 'owner',
+        syncFields: {
+          profilePicture: true,
+          displayName: true,
         },
       },
     },
