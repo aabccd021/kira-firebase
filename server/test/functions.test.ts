6375eed6dc63f1ff0f2755f8b4aed3872938ae4b
aabccd021 <aabccd021@gmail.com>
2021-08-09 14:04:47 +0700

    test functions

--- server/test/functions.test.ts
+++ server/test/functions.test.ts
@@ -1,26 +1,64 @@
-import { hello } from '../src/kira';
-import { firestore, functions, test } from './util';
+/* eslint-disable import/first */
+import * as admin from 'firebase-admin';
+const projectId = 'demo-kira-firebase';
+admin.initializeApp({ projectId });
+
+import * as functions from 'firebase-functions';
+import { QueryDocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+import * as functionsTest from 'firebase-functions-test';
+import { CreationTimeField } from 'kira-core';
+import { makeCountDraft, makeCreationTimeDraft, makeRefDraft } from 'kira-nosql';
+import { None } from 'trimop';
+
+import { getFirebaseTriggers } from '../src';
+
+const test = functionsTest({ projectId });
 
 describe('Unit tests', () => {
   afterAll(test.cleanup);
   it('tests a Cloud Firestore function', async () => {
-    const wrapped = test.wrap(
-      functions.firestore.document('/lowercase/{doc}').onCreate((doc) =>
-        firestore
-          .collection('uppercase')
-          .doc(doc.id)
-          .set({ text: hello(doc.data()['text']) })
+    const triggers = getFirebaseTriggers({
+      buildDraft: ({ spec, context }) => {
+        if (spec._type === 'Count') {
+          return makeCountDraft({ context, spec });
+        }
+        if (spec._type === 'Ref') {
+          return makeRefDraft({ context, spec });
+        }
+        if (spec._type === 'CreationTime') {
+          return makeCreationTimeDraft({ context, spec });
+        }
+        return {
+          onCreate: None(),
+          onDelete: None(),
+          onUpdate: None(),
+        };
+      },
+      firestore: admin.firestore(),
+      firestoreFieldValue: admin.firestore.FieldValue,
+      spec: {
+        user: {
+          joinedTime: CreationTimeField(),
+        },
+      },
+    });
+    const userOnCreateTrigger = triggers['user']?.onCreate;
+    expect(userOnCreateTrigger).toBeDefined();
+    const wrappedUserOnCreateTrigger = test.wrap(
+      userOnCreateTrigger as functions.CloudFunction<QueryDocumentSnapshot>
+    );
+    await wrappedUserOnCreateTrigger(
+      test.firestore.makeDocumentSnapshot(
+        {
+          _fromClient: true,
+          aab: 'ccd',
+        },
+        '/user/user1'
       )
     );
 
-    // Make a fake document snapshot to pass to the function
-    const after = test.firestore.makeDocumentSnapshot({ text: 'kira' }, '/lowercase/foo');
-
-    // Call the function
-    await wrapped(after);
-
     // Check the data in the Firestore emulator
-    const snap = await firestore.doc('/uppercase/foo').get();
+    const snap = await admin.firestore().doc('/user/user1').get();
     expect(snap.data()).toStrictEqual({ text: 'hallo kira' });
   }, 5000);
 });
